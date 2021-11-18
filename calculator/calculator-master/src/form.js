73618d1c1080324c448b25bde8e0950a7fbe1b45
revathiannapurna <62866592+revathiannapurna@users.noreply.github.com>
2021-11-18 09:46:29 +0530

    Add files via upload

---
+++ calculator/calculator-master/src/form.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+
+
+function myform(){
+    return(
+        <div className myform>
+            <form>
+      <label>Enter your name:
+        <input type="text"/>
+        
+      </label>
+      <input type="submit" />
+    </form>
+        </div>
+    )
+}
